6ad980bf452884267c89720b5bce8f784957d4ee
vvv <vvv@vvv.com>
2023-07-25 09:35:09 +0800

    update info

--- task/member_task.go
+++ task/member_task.go
@@ -1,7 +1,6 @@
 package task
 
 import (
-	"fmt"
 	"go-speed/global"
 	"go-speed/model"
 	"time"
@@ -10,7 +9,7 @@ import (
 // MemberLevelExpiredTask 会员等级到期任务（变更会员等级）
 func MemberLevelExpiredTask() {
 	global.Recovery()
-	global.Logger.Info().Msg("OrderExpiredTask start...")
+	global.Logger.Info().Msg("MemberLevelExpiredTask start...")
 	var err error
 	for {
 		nowTime := time.Now().Unix()
@@ -18,6 +17,7 @@ func MemberLevelExpiredTask() {
 		var list []*model.TUser
 		err = global.Db.Where("v2ray_tag = 1 and expired_time <= ?", afterDayTime).OrderBy("expired_time asc").Find(&list)
 		if err != nil {
+			global.Logger.Err(err).Msg("MemberLevelExpiredTask err")
 			time.Sleep(time.Second * 10)
 			continue
 		}
@@ -38,6 +38,12 @@ func MemberLevelExpiredTask() {
 }
 
 func needUpdateUser(user *model.TUser) bool {
+	currTimeSec := time.Now().Unix()
+	if user.ExpiredTime <= currTimeSec {
+		global.Logger.Info().Msgf("用户[%s]会员过期:", user.Uname)
+		//远程调用执行v2ray服务器删除UUID；得到回执后，并修改用户的v2ray_tag字段为2
+		return true
+	}
 	return false
 }
 
@@ -45,9 +51,39 @@ func needUpdateUser(user *model.TUser) bool {
 func MemberSpeedExpiredTask() {
 	global.Recovery()
 	global.Logger.Info().Msg("MemberSpeedExpiredTask start...")
+	var err error
 	for {
 		nowTime := time.Now().Unix()
-		fmt.Println(nowTime)
-		time.Sleep(time.Second * 30)
+		afterDayTime := nowTime + 24*3600
+		var list []*model.TSuccessRecord
+		err = global.Db.Where("end_time <= ? and status = 1", afterDayTime).OrderBy("end_time asc").Find(&list)
+		if err != nil {
+			global.Logger.Err(err).Msg("MemberSpeedExpiredTask err")
+			time.Sleep(time.Second * 10)
+			continue
+		}
+		if len(list) == 0 {
+			time.Sleep(time.Minute)
+			continue
+		}
+		for _, item := range list {
+			for {
+				if needUpdateLevelRecord(item) {
+					break
+				}
+				time.Sleep(time.Second)
+			}
+		}
+		time.Sleep(time.Second * 10)
 	}
+}
+
+func needUpdateLevelRecord(record *model.TSuccessRecord) bool {
+	currTimeSec := time.Now().Unix()
+	if record.EndTime <= currTimeSec {
+		global.Logger.Info().Msgf("套餐记录[%d]会员过期:", record.Id)
+		//执行过期流程
+		return true
+	}
+	return false
 }
