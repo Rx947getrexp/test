b2718d07629f201c02a21817e87f3be5a7916fcb
vvv <vvv@vvv.com>
2023-07-31 17:42:08 +0800

    update job test

--- cmd/go-job/main.go
+++ cmd/go-job/main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"go-speed/global"
 	"go-speed/initialize"
+	"time"
 )
 
 func main() {
 	initialize.InitComponents()
+	go a()
+	go b()
 	engine := initialize.JobRouters()
 	if err := engine.Run(":" + global.Config.System.Addr); err != nil {
 		global.Logger.Err(err).Msg("启动失败")
 		return
 	}
+}
+
+func a() {
+	fmt.Println("当前时间戳1：", time.Now().Unix())
+}
+
+func b() {
+	fmt.Println("当前时间戳2：", time.Now().Unix())
 }
