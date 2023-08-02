39535fce5757235c8fe5c9698a56187c784f5b50
Administrator <Administrator@qq.com>
2023-08-02 16:48:47 +0800

    测试打印请求参数

--- router/api.go
+++ router/api.go
@@ -6,45 +6,45 @@ import (
 )
 
 func ApiRoute(group *gin.RouterGroup) {
-	group.Use(api.PrintParam())
+	//group.Use(api.PrintParam())
+	//{}
+	group.POST("generate_dev_id", api.GenerateDevId)
+	group.POST("send_email", api.SendEmail)
+	group.POST("reg", api.Reg)
+	group.POST("login", api.Login)
+	group.POST("forget_passwd", api.ForgetPasswd)
+
+	group.GET("notice_list", api.NoticeList)
+	group.GET("notice_detail", api.NoticeDetail)
+	group.GET("node_list", api.NodeList)
+	group.GET("dns_list", api.DnsList)
+	group.GET("combo_list", api.ComboList)
+	group.GET("ad_list", api.AdList)
+	group.GET("app_info", api.AppInfo)
+	group.GET("app_filter", api.AppFilter)
+
+	group.Use(api.JWTAuth())
 	{
-		group.POST("generate_dev_id", api.GenerateDevId)
-		group.POST("send_email", api.SendEmail)
-		group.POST("reg", api.Reg)
-		group.POST("login", api.Login)
-		group.POST("forget_passwd", api.ForgetPasswd)
+		group.POST("change_passwd", api.ChangePasswd)
+		group.GET("user_info", api.UserInfo)
 
-		group.GET("notice_list", api.NoticeList)
-		group.GET("notice_detail", api.NoticeDetail)
-		group.GET("node_list", api.NodeList)
-		group.GET("dns_list", api.DnsList)
-		group.GET("combo_list", api.ComboList)
-		group.GET("ad_list", api.AdList)
-		group.GET("app_info", api.AppInfo)
-		group.GET("app_filter", api.AppFilter)
+		group.GET("team_list", api.TeamList)
+		group.GET("team_info", api.TeamInfo)
 
-		group.Use(api.JWTAuth())
-		{
-			group.POST("change_passwd", api.ChangePasswd)
-			group.GET("user_info", api.UserInfo)
+		group.POST("receive_free", api.ReceiveFree)
+		group.GET("receive_free_summary", api.ReceiveFreeSummary)
 
-			group.GET("team_list", api.TeamList)
-			group.GET("team_info", api.TeamInfo)
+		group.POST("upload_log", api.UploadLog)
 
-			group.POST("receive_free", api.ReceiveFree)
-			group.GET("receive_free_summary", api.ReceiveFreeSummary)
+		group.POST("create_order", api.CreateOrder)
+		group.GET("order_list", api.OrderList)
 
-			group.POST("upload_log", api.UploadLog)
+		group.GET("dev_list", api.DevList)
+		group.POST("ban_dev", api.BanDev)
 
-			group.POST("create_order", api.CreateOrder)
-			group.GET("order_list", api.OrderList)
+		group.POST("change_network", api.ChangeNetwork) //暂没用到
+		group.POST("connect", api.Connect)
 
-			group.GET("dev_list", api.DevList)
-			group.POST("ban_dev", api.BanDev)
-
-			group.POST("change_network", api.ChangeNetwork) //暂没用到
-			group.POST("connect", api.Connect)
-
-		}
 	}
+
 }
