4778f3814e02aedc4a8ed5a6e9aea37dd5772e31
fredhu2023 <china_cn@126.com>
2023-08-10 11:51:16 +0800

    UUI

--- api/executor/executor.go
+++ api/executor/executor.go
@@ -2,6 +2,7 @@ package executor
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"go-speed/constant"
@@ -74,24 +75,35 @@ func AddSub(c *gin.Context) {
 	if param.Tag == "1" {
 		_ = os.Remove(fmt.Sprintf("/v2rayJsonSub/%s.json", param.Uuid))
 		cmds := exec.Command("/usr/local/bin/v2ray", "  api adi -s 127.0.0.1:10085 /v2rayJsonAdd")
-		out, err := cmds.CombinedOutput()
+
+		var stdout, stderr bytes.Buffer
+		cmds.Stdout = &stdout // 标准输出
+		cmds.Stderr = &stderr // 标准错误
+		err := cmds.Run()
+		outStr, errStr := string(stdout.Bytes()), string(stderr.Bytes())
+		fmt.Printf("out:\n%s\nerr:\n%s\n", outStr, errStr)
 		if err != nil {
 			global.Logger.Err(err).Msg("添加失败")
 			response.ResFail(c, "添加udid启动失败")
 			return
 		}
 
-		fmt.Printf("combined out:%s, Email:%s,uuid:%s,Tag:%s", string(out), email, name, param.Tag)
+		fmt.Printf("combined out:%s, Email:%s,uuid:%s,Tag:%s", outStr, email, name, param.Tag)
 	} else {
 		_ = os.Remove(fmt.Sprintf("/v2rayJsonAdd/%s.json", param.Uuid))
 		cmds := exec.Command("/usr/local/bin/v2ray", "  api rmi -s 127.0.0.1:10085 /v2rayJsonSub")
-		out, err := cmds.CombinedOutput()
+		var stdout, stderr bytes.Buffer
+		cmds.Stdout = &stdout // 标准输出
+		cmds.Stderr = &stderr // 标准错误
+		err := cmds.Run()
+		outStr, errStr := string(stdout.Bytes()), string(stderr.Bytes())
+		fmt.Printf("out:\n%s\nerr:\n%s\n", outStr, errStr)
 		if err != nil {
 			global.Logger.Err(err).Msg("删除udid启动失败")
 			response.ResFail(c, "删除udid启动失败")
 			return
 		}
-		fmt.Printf("delete combined out:%s, Email:%s,uuid:%s,Tag:%s", string(out), email, name, param.Tag)
+		fmt.Printf("delete combined out:%s, Email:%s,uuid:%s,Tag:%s", outStr, email, name, param.Tag)
 	}
 	global.Logger.Info().Msg("添加成功")
 	_ = os.Remove(path)
