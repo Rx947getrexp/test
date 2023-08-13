c4eaa218937e084ba5f7120cbe0ff193b1b88632
fredhu2023 <china_cn@126.com>
2023-08-13 14:29:11 +0800

    恢复版本

--- api/executor/executor.go
+++ api/executor/executor.go
@@ -43,7 +43,7 @@ func AddSub(c *gin.Context) {
 		response.ResFail(c, "参数错误")
 		return
 	}
-	if param.Uuid == "c541b521-17dd-11ee-bc4e-0c9d92c013fb" {
+	if param.Uuid == "c541b521-17dd-11ee-bc4e-0c9d92c013fb" || param.Uuid == "bf268a88-318f-d58f-0e9f-66d6f066be31" {
 		response.ResOk(c, "成功")
 	}
 	path := ""
