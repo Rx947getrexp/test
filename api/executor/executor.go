8ab517a0d376c98489ee40c97ed6de5adcda8935
win20232026 <win2023bar@outlook.com>
2023-10-07 15:55:47 +0800

    rb

--- api/executor/executor.go
+++ api/executor/executor.go
@@ -49,6 +49,17 @@ func AddSub(c *gin.Context) {
 	if param.Tag == "1" {
 		v2rayJson = fmt.Sprintf("{\"email\": \"%s\",\"password\": \"%s\"}", param.Email, param.Uuid)
 		path = fmt.Sprintf("/v2rayJsonAdd/%s.json", name)
+		_, err := os.Stat(path)
+
+		//isnotexist来判断，是不是不存在的错误
+		if os.IsNotExist(err) { //如果返回的错误类型使用os.isNotExist()判断为true，说明文件或者文件夹不存在
+			fmt.Printf(path + " is not exists")
+		} else {
+			fmt.Printf(path + " is exists")
+			response.ResOk(c, "成功")
+			return
+		}
+
 		file, err := os.OpenFile(path, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
 		wr := bufio.NewWriter(file)
 		fmt.Printf(v2rayJson)
