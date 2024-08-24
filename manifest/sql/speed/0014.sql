fdee6eeb34f3a71a8a1bba4b75d85dd03a99358f
LKyoona <438396211@qq.com>
2024-08-24 17:31:59 +0800

    Update 0014.sql

--- manifest/sql/speed/0014.sql
+++ manifest/sql/speed/0014.sql
@@ -1,1 +1,2 @@
 ALTER TABLE `t_user_op_log` ADD COLUMN version VARCHAR(64) AFTER content;
+ALTER TABLE admin_user ADD COLUMN `channel` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '可查看渠道范围,为空则可查看所有范围'
