bc514593172e1e6a34ea09e14ba975444f7c78f2
LKyoona <438396211@qq.com>
2024-09-17 12:52:13 +0800

    Update util.py

--- shell/crontab/report/util.py
+++ shell/crontab/report/util.py
@@ -1,23 +1,16 @@
 # -*- coding: utf-8 -*-
-import os
-import subprocess
-import sys
-import time
-import fcntl
 from datetime import datetime, timedelta
 import geoip2.database
-# -*- coding: utf-8 -*-
-import os
-import subprocess
-import sys
-import time
-import fcntl
-from datetime import datetime, timedelta
-import geoip2.database
+
+
 def get_month_date():
     # 获取今天的日期
     today = datetime.now()
     # 判断今天是否是1号
+    # 获取今天的日期字符串
+    #today_str = "2024-09-12 00:00:00"
+    # 将字符串转换为 datetime 对象
+    #today = datetime.strptime(today_str, "%Y-%m-%d %H:%M:%S")
     if today.day == 1:
         # 获取上个月的第一天
         first_day_last_month = (today.replace(day=1) - timedelta(days=1)).replace(day=1)
@@ -31,9 +24,11 @@ def get_yesterday_date():
     today = datetime.now()
     # 计算昨天的日期
     yesterday = today - timedelta(days=1)
-
     # 将昨天的日期格式化为 "2024-01-01" 格式
     return yesterday.strftime('%Y-%m-%d')
+    #DATE=input("请输入：")
+    #return DATE
+    #return "2024-09-10"
 
 
 def time_format(s):
@@ -50,45 +45,6 @@ class Time:
 
     def get_end_time(self):
         return self.date + " 23:59:59"
-
-
-
-class IpSearch:
-    def __init__(self):
-        self.reader = geoip2.database.Reader('/shell/report/GeoLite2-City.mmdb')
-
-    def get_location(self, ip_address):
-        response = self.reader.city(ip_address)
-        Country_IsoCode = response.country.iso_code
-        if (Country_IsoCode == None):
-            Country_IsoCode = "None"
-        return Country_IsoCode
-
-def get_yesterday_date():
-    # 获取今天的日期
-    today = datetime.now()
-    # 计算昨天的日期
-    yesterday = today - timedelta(days=1)
-
-    # 将昨天的日期格式化为 "2024-01-01" 格式
-    return yesterday.strftime('%Y-%m-%d')
-
-
-def time_format(s):
-    return datetime.strptime(s, "%Y-%m-%d %H:%M:%S")
-
-
-class Time:
-    def __init__(self, date):
-        self.class_name = "util.Time"
-        self.date = date
-
-    def get_start_time(self):
-        return self.date + " 00:00:00"
-
-    def get_end_time(self):
-        return self.date + " 23:59:59"
-
 
 
 class IpSearch:
