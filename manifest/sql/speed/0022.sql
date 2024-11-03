40d1c56a82a25aa044701c9fcef19c94a97522f6
shenfuqing <shenfuqing@163.com>
2024-11-04 02:48:22 +0800

    fix-20241103

---
+++ manifest/sql/speed/0022.sql
@@ -0,0 +1,10 @@
+CREATE TABLE t_user_report_monthly (
+    id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+    stat_month  INT(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT '统计月份',
+    os VARCHAR(128) NOT NULL COMMENT '设备类型',
+    user_count INT(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT '用户总数',
+    new_users INT(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT '新增用户量',
+    retained_users INT(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT '次月留存',
+    created_at TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP COMMENT '记录创建时间',
+    PRIMARY KEY (id)
+)
