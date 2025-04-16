c9f937ee57b7ed1cb2b621358dd2ce2f859a4a87
dev-001 <dev-001@mail.com>
2025-04-16 23:30:01 +0800

    tools

--- cmd/go-tools/config/cmd.go
+++ cmd/go-tools/config/cmd.go
@@ -1,9 +1,12 @@
 package config
 
 import (
+	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/cobra"
 	"go-speed/cmd/go-tools/common"
+	"go-speed/dao"
+	"go-speed/model/entity"
 	"go-speed/service"
 	"log"
 	"net/http/httptest"
@@ -11,7 +14,8 @@ import (
 )
 
 const (
-	CmdTypeCheck = "check"
+	CmdTypeCheck            = "check"
+	CmdTypeCheckConfigAndDB = "check-config-and-db"
 )
 
 var (
@@ -28,7 +32,7 @@ func NewCommand() *cobra.Command {
 			return runCommand()
 		},
 	}
-	command.Flags().StringVarP(&cmdAction, "action", "a", "check", "操作类型")
+	command.Flags().StringVarP(&cmdAction, "action", "a", "check", "操作类型(check|check-config-and-db)")
 	command.Flags().StringVarP(&ip, "ip", "i", "", "机器IP")
 	return command
 }
@@ -43,6 +47,8 @@ func runCommand() (err error) {
 		switch cmdAction {
 		case CmdTypeCheck:
 			checkV2rayConfig(ctx, nodeIp)
+		case CmdTypeCheckConfigAndDB:
+			checkV2rayConfigAndDB(ctx, nodeIp)
 		default:
 			log.Fatalf("cmdAction %s invalid", cmdAction)
 		}
@@ -66,5 +72,67 @@ func checkV2rayConfig(ctx *gin.Context, nodeIp string) {
 			m[strings.ToLower(item.Email)] = struct{}{}
 		}
 	}
-	log.Printf("nodeIp: %s check finished\n", nodeIp)
+	log.Printf("nodeIp: %s check finished. len(items): %d\n", nodeIp, len(resp.Items))
+}
+
+func checkV2rayConfigAndDB(ctx *gin.Context, nodeIp string) {
+	resp, err := service.GetUserListFromNode(ctx, nodeIp)
+	if err != nil {
+		log.Fatalf("%s GetUserListFromNode failed, err: %s", nodeIp, err.Error())
+	}
+
+	if nodeIp == "154.93.104.115" {
+		out, _ := json.Marshal(resp)
+		log.Printf("resp: %s\n", string(out))
+	}
+
+	m := make(map[string]struct{})
+	configUser := make(map[string]string)
+	var emails []string
+	for _, item := range resp.Items {
+
+		email := strings.ToLower(item.Email)
+		if _, ok := m[email]; ok {
+			log.Printf("%s %s, dumplicate\n", nodeIp, email)
+		} else {
+			m[strings.ToLower(item.Email)] = struct{}{}
+		}
+
+		if _, ok := configUser[item.Email]; ok {
+			log.Printf("%s %s, dumplicate configUser\n", nodeIp, email)
+		} else {
+			configUser[item.Email] = item.Password
+			emails = append(emails, item.Email)
+		}
+	}
+	var users []entity.TUser
+	err = dao.TUser.Ctx(ctx).WhereIn(dao.TUser.Columns().Email, emails).Scan(&users)
+	if err != nil {
+		log.Printf("%s query user from db failed, err: %s", nodeIp, err.Error())
+	} else {
+		dbUser := make(map[string]string)
+		for _, user := range users {
+			if _, ok := dbUser[user.Email]; ok {
+				log.Printf("%s, dumplicate db user info\n", user.Email)
+			} else {
+				dbUser[user.Email] = user.V2RayUuid
+			}
+		}
+		for email, uuid := range configUser {
+			if dbUuid, ok := dbUser[email]; ok {
+				if uuid != dbUuid {
+					log.Printf("%s %s, config uuid is diff to db user info\n", nodeIp, email)
+				}
+			} else {
+				//log.Printf("%s %s, config user can not find in db user info\n", nodeIp, email)
+			}
+		}
+		if nodeIp == "154.93.104.115" {
+			out1, _ := json.Marshal(configUser)
+			out2, _ := json.Marshal(dbUser)
+			log.Printf("configUser: %s\n", string(out1))
+			log.Printf("dbUser: %s\n", string(out2))
+		}
+	}
+	log.Printf("nodeIp: %s check finished. len(items): %d\n", nodeIp, len(resp.Items))
 }
