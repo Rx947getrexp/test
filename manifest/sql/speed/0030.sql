21bbb1098733a4b5dda20a9111a60b5a4b8f650b
Alvaro丶Soler <shenfuqing@163.com>
2025-04-24 10:21:05 +0800

    优化官网下载映射

--- manifest/sql/speed/0030.sql
+++ manifest/sql/speed/0030.sql
@@ -1,7 +1,7 @@
 -- 推广dns域名映射表
 CREATE TABLE IF NOT EXISTS `speed`.`t_promotion_dns` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增id',
-  `dns` varchar(64) DEFAULT NULL COMMENT '域名',
+  `dns` varchar(128) NOT NULL DEFAULT "" COMMENT '域名',
   `ip` varchar(64) DEFAULT NULL COMMENT 'ip地址',
   `mac_channel` varchar(64) DEFAULT NULL COMMENT '苹果电脑渠道',
   `win_channel` varchar(64) DEFAULT NULL COMMENT 'windows电脑渠道',
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS `speed`.`t_promotion_dns` (
   `status` TINYINT(1) NOT NULL DEFAULT 1 COMMENT '状态:1-正常；2-已软删',
   `created_at` timestamp NULL DEFAULT NULL COMMENT '创建时间',
   `updated_at` timestamp NULL DEFAULT NULL COMMENT '更新时间',
-  `author` varchar(255) DEFAULT NULL COMMENT '作者',
+  `author` varchar(64) DEFAULT NULL COMMENT '作者',
   `comment` varchar(255) DEFAULT NULL COMMENT '备注信息',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='推广dns域名映射表';
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS `speed`.`t_promotion_dns` (
 -- app商店管理
 CREATE TABLE IF NOT EXISTS `speed`.`t_app_store` (
   `id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT '自增id',
-  `title_cn` VARCHAR(64) DEFAULT NULL COMMENT '商店名称(中文)',
+  `title_cn` VARCHAR() DEFAULT NULL COMMENT '商店名称(中文)',
   `title_en` VARCHAR(64) DEFAULT NULL COMMENT '商店名称(英文)',
   `title_ru` VARCHAR(64) DEFAULT NULL COMMENT '商店名称(俄语)',
   `type` VARCHAR(64) DEFAULT NULL COMMENT '商店类型，ios(苹果)，android(安卓)...',
@@ -27,7 +27,7 @@ CREATE TABLE IF NOT EXISTS `speed`.`t_app_store` (
   `status` TINYINT(1) NOT NULL DEFAULT 1 COMMENT '状态:1-正常；2-已软删',
   `created_at` TIMESTAMP NULL DEFAULT NULL COMMENT '创建时间',
   `updated_at` TIMESTAMP NULL DEFAULT NULL COMMENT '更新时间',
-  `author` VARCHAR(255) DEFAULT NULL COMMENT '作者',
+  `author` VARCHAR(64) DEFAULT NULL COMMENT '作者',
   `comment` VARCHAR(255) DEFAULT NULL COMMENT '备注信息',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='app商店管理表';
